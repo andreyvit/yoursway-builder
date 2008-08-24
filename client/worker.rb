1ebc8afa8699a28f4eb41b7e69d7b094b35fa37a
Andrey Tarantsov <andreyvit@gmail.com>
2008-08-24 20:22:09 +0700

    Changed protocol wording a bit, added protocol version number.

--- client/worker.rb
+++ client/worker.rb
@@ -48,23 +48,41 @@ while not interrupted
   if res.code.to_i != 200
     log "Error response: code #{res.code}"
   else
-    res.body.split("\n").each do |line|
-      next if (line = line.strip).empty?
-      command, *args = line.split("\t")
-      case command.upcase
-      when 'SETPOLL'
-        new_interval = [60*20, args[0].to_i].min
-        if new_interval >= 30 && config.poll_interval != new_interval
-          config.poll_interval = new_interval
-          log "Poll interval set to #{config.poll_interval}"
+    first_line, *other_lines = res.body.split("\n")
+
+    result = []
+    message_id = nil
+    
+    command, *args = first_line.split("\t")
+    command.upcase!
+    if ['IDLE', 'ENVELOPE'].include?(command)
+      proto_ver = args[0]
+      if proto_ver == 'v1'
+        if command == 'IDLE'
+          new_interval = [60*20, args[1].to_i].min
+          if new_interval >= 30 && config.poll_interval != new_interval
+            config.poll_interval = new_interval
+            log "Poll interval set to #{config.poll_interval}"
+          end
+          other_lines = []  # never execute
+        else
+          message_id = args[1]
+          wait_before_polling = false
         end
-      when 'SAY'
-        log "Saying #{args[0]}"
-        invoke('say', args[0])
-        wait_before_polling = false
-      else
-        log "Unknown command #{command}(#{args.join(', ')})"
       end
+      
+      other_lines.each do |line|
+        next if (line = line.strip).empty?
+        command, *args = line.split("\t")
+        case command.upcase
+        when 'SAY'
+          log "Saying #{args[0]}"
+          invoke('say', args[0])
+        else
+          log "Unknown command #{command}(#{args.join(', ')})"
+        end
+      end
+      
     end
   end
   
--- server/main.py
+++ server/main.py
@@ -269,11 +269,11 @@ class ObtainWorkHandler(BaseHandler):
     builder.put()
     message = builder.messages.order('created_at').get()
     if message == None:
-      self.response.out.write("SETPOLL\t%d" % self.config.builder_poll_interval)
+      self.response.out.write("IDLE\tv1\t%d" % self.config.builder_poll_interval)
     else:
       message.state = 1
       message.put()
-      body = "JOBID\t%s\n%s" % (message.key(), message.body)
+      body = "ENVELOPE\tv1\t%s\n%s" % (message.key(), message.body)
       self.response.out.write(body)
 
 class ServerConfigHandler(BaseHandler):
